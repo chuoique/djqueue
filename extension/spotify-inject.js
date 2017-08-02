e6f1475f93926fd536cc359a3f2cee5ca91b47ba
Daniel Johnson <danielj41@gmail.com>
2017-08-01 22:07:39 -0700

    spotify: autoplay chrome extension: make it work again
    The old code didn't work any more.
    
    That's what I get for relying on "undocumented apis" :)
    
    Tweak it around a bit so that it clicks the play button at the
    right time.

--- extension/spotify-inject.js
+++ extension/spotify-inject.js
@@ -1,22 +1,24 @@
 (function() {
-  // force new songs to play, even if one is already playing.
+  // wait for `status.json`'s response before attempting to autoplay the song.
   // http://stackoverflow.com/questions/7775767/javascript-overriding-xmlhttprequest-open
+  var clicked = false;
   var proxied = window.XMLHttpRequest.prototype.open;
   window.XMLHttpRequest.prototype.open = function() {
     var newArgs = [].slice.call(arguments);
-    
+
     if(newArgs[1].indexOf('status.json') !== -1 && this.onload) {
       var onload = this.onload;
       this.onload = function() {
         onload.apply(this, [].slice.call(arguments));
 
-        var data = JSON.parse(this.responseText);
-        if(data.playing && data.playing === true) {
-          setTimeout(function() {
-            document.querySelector('#item-action').click();
-          }, 0);
-        }
-        
+        setTimeout(function() {
+          if (clicked) {
+            return;
+          }
+          document.querySelector('#play-button').click();
+          clicked = true;
+        }, 0);
+
         window.XMLHttpRequest.prototype.open = proxied;
       };
     }
